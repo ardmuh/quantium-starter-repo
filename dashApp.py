a8c7eef2e419960b35d9bbd48e7f82a7164496e3
ardmuh <amardi1419@gmail.com>
2022-10-23 09:56:23 +0700

    Task 4 - Improve Your Dash Application

--- dashApp.py
+++ dashApp.py
@@ -1,29 +1,63 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
 import pandas as pd
-from dash import Dash, html, dcc
-from plotly.express import line
 
-# load in data
-df = pd.read_csv('output.csv')
-df = df.sort_values(by="date")
+DATA_PATH = "./output.csv"
+COLORS = {
+    "bg-color": "#0d1117",
+    "font": "#F5F5F5",
+}
 
-# initialize dash
-dash_app = Dash(__name__)
+df = pd.read_csv(DATA_PATH, parse_dates=['date'], dayfirst=True)
+df['date'] = df['date'].dt.to_period('M')
+df = df.groupby([df['date'].astype(str), 'region'])['sales'].sum()
 
-# create the visualization
-line_chart = line(df, x="date", y="sales", title="Pink Morsel Sales")
-visualization = dcc.Graph(id="visualization", figure=line_chart)
+app = Dash(__name__)
 
-# create the header
-header = html.H1("Pink Morsel Visualizer",id="header")
+app.layout = html.Div([
+    html.H1(["Pink Morsel Visualizer"], id = 'header', style={
+        "background-color": COLORS["bg-color"],
+        "color": COLORS["font"],
+        "border-radius": "20px"}),
+    
+     html.Div(dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'north', 'value':'north' },
+            {'label': 'south', 'value':'south'},
+            {'label': 'east', 'value':'east'},
+            {'label': 'west', 'value':'west'},
+            {'label': 'all', 'value':'all'}
+            ],
+        value = 'all')),
 
-# define the app layout
-dash_app.layout = html.Div(
-    [
-        header,
-        visualization
-    ]
-)
+    html.Div(dcc.Graph(id='salesGraph'),
+             style={
+                "background-color": "#708090",
+             })
 
-# this is only true if the module is executed as the program entrypoint
+], style={'width': '100%',
+            "textAlign": "center",
+            'display': 'inline-block', 
+             "background-color": COLORS["bg-color"],
+             "border-radius": "20px"})
+
+
+@app.callback(
+    Output('salesGraph', 'figure'),
+    Input('dropdown', 'value'))
+
+def update_graph(dropdown):
+    if dropdown == 'all':
+        dff = df
+    else:
+        dff = df[df.index.get_level_values('region').isin([dropdown])]
+
+    fig = px.line(dff, x=dff.index.get_level_values(0), y='sales', color=dff.index.get_level_values('region'))
+    fig.update_xaxes(title='Date')
+    fig.update_layout(transition_duration=1000)
+
+    return fig
+
+
 if __name__ == '__main__':
-    dash_app.run_server()
+    app.run_server(debug=True)
